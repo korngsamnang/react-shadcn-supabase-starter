ff21e19c8cbae3470acec309483a81ea07dcca78
Korng Samnang <korngsamnang168@gmail.com>
2024-12-27 14:51:45 +0700

    init: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button.jsx";
+
+const App = () => {
+    return (
+        <div>
+            Hello React!!
+            <Button>Click Me!!!</Button>
+        </div>
+    );
+};
+
+export default App;
